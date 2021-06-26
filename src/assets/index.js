959c39b6ebc9fe2cf94fd73baa020b05dc2fa815
Dubchak Marina <dubchmar@gmail.com>
2021-06-27 00:02:23 +0300

    added first settings to project

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+/* Icons */
+export {default as calendar} from './icons/calendar.svg';
+export {default as design} from './icons/design.svg';
+export {default as devices} from './icons/devices.svg';
+export {default as secure} from './icons/secure.svg';
+
+/* Images */
+export {default as backgroundTop} from './images/backgroud-top.svg';
+export {default as backgroundBottom} from './images/background-bottom.svg';
+export {default as mobile} from './images/mobile.png';
